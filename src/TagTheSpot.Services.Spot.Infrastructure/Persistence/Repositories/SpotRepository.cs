0e7f6ec98f8f874e04e29bb1d563ddd42a8b5bff
Savchenko Artem <p.savchenkoad.back@gmail.com>
2025-07-26 16:36:54 +0300

    [TTS-40] Implement Get Spot By Id endpoint (#11)
    Co-authored-by: Oleh Pobyvanets <pobyvanets2005@gmail.com>

---
+++ src/TagTheSpot.Services.Spot.Infrastructure/Persistence/Repositories/SpotRepository.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TagTheSpot.Services.Spot.Domain.Spots;
+
+namespace TagTheSpot.Services.Spot.Infrastructure.Persistence.Repositories
+{
+    public class SpotRepository : ISpotRepository
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public SpotRepository(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<Domain.Spots.Spot?> GetByIdAsync(Guid id)
+        {
+            return await _dbContext.Spots.FirstOrDefaultAsync(spot => spot.Id == id);
+        }
+    }
+}
