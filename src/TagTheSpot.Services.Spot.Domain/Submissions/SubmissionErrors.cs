d0695a02fe32b3299549fd7735e107e9d77d37bd
savchenkoaddev <p.savchenkoad.dev@gmail.com>
2025-09-03 08:15:27 +0300

    Implement the endpoint

---
+++ src/TagTheSpot.Services.Spot.Domain/Submissions/SubmissionErrors.cs
@@ -0,0 +1,12 @@
+﻿using TagTheSpot.Services.Shared.Essentials.Results;
+
+namespace TagTheSpot.Services.Spot.Domain.Submissions
+{
+    public static class SubmissionErrors
+    {
+        public static readonly Error NotFound =
+            Error.NotFound(
+                code: "Submission.NotFound",
+                description: "The submission has not been found.");
+    }
+}
